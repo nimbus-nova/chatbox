980b82c7ee2444c947acdea35c36874e5bb83aec
Andy <andy985897924@gmail.com>
2024-07-23 23:39:23 -0700

    update NDK

--- setup_mlc.sh
+++ setup_mlc.sh
@@ -1,5 +1,5 @@
 source $HOME/.cargo/env # Rust
-export ANDROID_NDK=$HOME/Android/Sdk/ndk/26.1.10909125 # TODO: update to whatever SDK you have here
+export ANDROID_NDK=$HOME/Android/Sdk/ndk/27.0.12077973 # TODO: update to whatever SDK you have here
 export TVM_NDK_CC=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang
 export JAVA_HOME=$HOME/android-studio/jbr
 export TVM_SOURCE_DIR=$HOME/mlc-llm/3rdparty/tvm
